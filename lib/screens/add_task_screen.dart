dc0b248122e719b61b5eee90ff512fc9c6e9bae3
quocthai25 <129527355+quocthai25@users.noreply.github.com>
2024-09-28 03:05:41 +0700

    xoa anh

--- lib/screens/add_task_screen.dart
+++ lib/screens/add_task_screen.dart
@@ -38,83 +38,165 @@ class _AddTaskScreenState extends State<AddTaskScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Thêm nhiệm vụ mới'),
+        flexibleSpace: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color(0xffB81736),
+                Color(0xff281537),
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+        ),
+        title: const Text('Thêm nhiệm vụ mới',style: TextStyle(color: Colors.white),),
         centerTitle: true,
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Form(
-          key: _formKey,
-          child: ListView(
-            children: [
-              ListTile(
-                title: Text(_date == null ? 'Chọn ngày' : 'Ngày: $_date'),
-                trailing: const Icon(Icons.calendar_today),
-                onTap: () => _selectDate(context),
-              ),
-              TextFormField(
-                decoration: const InputDecoration(labelText: 'Nội dung nhiệm vụ'),
-                onChanged: (value) => _content = value,
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Vui lòng nhập nội dung nhiệm vụ';
-                  }
-                  return null;
-                },
-              ),
-              TextFormField(
-                decoration: const InputDecoration(labelText: 'Thời gian'),
-                onChanged: (value) => _time = value,
-              ),
-              TextFormField(
-                decoration: const InputDecoration(labelText: 'Vị trí'),
-                onChanged: (value) => _location = value,
-              ),
-              TextFormField(
-                decoration: const InputDecoration(labelText: 'Chủ trì'),
-                onChanged: (value) => _chairperson = value,
-              ),
-              TextFormField(
-                decoration: const InputDecoration(labelText: 'Ghi chú'),
-                onChanged: (value) => _notes = value,
-              ),
-              DropdownButtonFormField<String>(
-                decoration: const InputDecoration(labelText: 'Trạng thái nhiệm vụ'),
-                value: _status,
-                onChanged: (newValue) {
-                  setState(() {
-                    _status = newValue!;
-                  });
-                },
-                items: _statuses
-                    .map((status) =>
-                        DropdownMenuItem(value: status, child: Text(status)))
-                    .toList(),
-              ),
-              TextFormField(
-                decoration: const InputDecoration(labelText: 'Người phê duyệt'),
-                onChanged: (value) => _approvalPerson = value,
-              ),
-              const SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: () {
-                  if (_formKey.currentState!.validate()) {
-                    Navigator.pop(context, {
-                      'date': _date,
-                      'content': _content,
-                      'time': _time,
-                      'location': _location,
-                      'chairperson': _chairperson,
-                      'notes': _notes,
-                      'status': _status,
-                      'approvalPerson': _approvalPerson,
-                    });
-                  }
-                },
-                child: const Text('Lưu nhiệm vụ'),
-              ),
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              Color(0xffB81736),
+              Color(0xff281537),
             ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
           ),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Form(
+            key: _formKey,
+            child: ListView(
+              children: [
+                _buildCard(
+                  child: ListTile(
+                    title: Text(
+                      _date == null ? 'Chọn ngày' : 'Ngày: $_date',
+                      style: const TextStyle(
+                          fontSize: 16, fontWeight: FontWeight.w500),
+                    ),
+                    trailing: const Icon(Icons.calendar_today),
+                    onTap: () => _selectDate(context),
+                  ),
+                ),
+                const SizedBox(height: 16),
+                _buildTextFormField(
+                  label: 'Nội dung nhiệm vụ',
+                  onChanged: (value) => _content = value,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Vui lòng nhập nội dung nhiệm vụ';
+                    }
+                    return null;
+                  },
+                ),
+                _buildTextFormField(
+                  label: 'Thời gian',
+                  onChanged: (value) => _time = value,
+                ),
+                _buildTextFormField(
+                  label: 'Vị trí',
+                  onChanged: (value) => _location = value,
+                ),
+                _buildTextFormField(
+                  label: 'Chủ trì',
+                  onChanged: (value) => _chairperson = value,
+                ),
+                _buildTextFormField(
+                  label: 'Ghi chú',
+                  onChanged: (value) => _notes = value,
+                ),
+                const SizedBox(height: 16),
+                _buildCard(
+                  child: DropdownButtonFormField<String>(
+                    decoration: const InputDecoration(
+                      labelText: 'Trạng thái nhiệm vụ',
+                      border: InputBorder.none,
+                    ),
+                    value: _status,
+                    onChanged: (newValue) {
+                      setState(() {
+                        _status = newValue!;
+                      });
+                    },
+                    items: _statuses
+                        .map((status) => DropdownMenuItem(
+                            value: status, child: Text(status)))
+                        .toList(),
+                  ),
+                ),
+                const SizedBox(height: 16),
+                _buildTextFormField(
+                  label: 'Người phê duyệt',
+                  onChanged: (value) => _approvalPerson = value,
+                ),
+                const SizedBox(height: 50),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.deepPurple,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    padding: const EdgeInsets.symmetric(vertical: 10),
+                  ),
+                  onPressed: () {
+                    if (_formKey.currentState!.validate()) {
+                      Navigator.pop(context, {
+                        'date': _date,
+                        'content': _content,
+                        'time': _time,
+                        'location': _location,
+                        'chairperson': _chairperson,
+                        'notes': _notes,
+                        'status': _status,
+                        'approvalPerson': _approvalPerson,
+                      });
+                    }
+                  },
+                  child: const Text(
+                    'Lưu nhiệm vụ',
+                    style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildCard({required Widget child}) {
+    return Card(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10),
+      ),
+      elevation: 3,
+      shadowColor: Colors.black.withOpacity(0.5),
+      child: child,
+    );
+  }
+
+  Widget _buildTextFormField({
+    required String label,
+    required Function(String) onChanged,
+    String? Function(String?)? validator,
+  }) {
+    return _buildCard(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16.0),
+        child: TextFormField(
+          decoration: InputDecoration(
+            labelText: label,
+            border: InputBorder.none,
+          ),
+          onChanged: onChanged,
+          validator: validator,
         ),
       ),
     );
--- lib/screens/task_detail_screen.dart
+++ lib/screens/task_detail_screen.dart
@@ -26,53 +26,108 @@ class _TaskDetailScreenState extends State<TaskDetailScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Chi tiết nhiệm vụ'),
-        actions: [
-          IconButton(
-            icon: const Icon(Icons.save),
-            onPressed: () {
-              Navigator.pop(context, {
-                'content': content,
-                'ngày': date,
-                'giờ': time,
-              });
-            },
+        title: const Text('Chi tiết nhiệm vụ',style: TextStyle(color: Colors.white),),
+        centerTitle: true,
+        flexibleSpace: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color(0xffB81736),
+                Color(0xff281537),
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
           ),
-        ],
+        ),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            TextField(
-              decoration: const InputDecoration(labelText: 'Nội dung'),
-              onChanged: (value) {
-                setState(() {
-                  content = value;
-                });
-              },
-              controller: TextEditingController(text: content),
-            ),
-            TextField(
-              decoration: const InputDecoration(labelText: 'Ngày'),
-              onChanged: (value) {
-                setState(() {
-                  date = value;
-                });
-              },
-              controller: TextEditingController(text: date),
-            ),
-            TextField(
-              decoration: const InputDecoration(labelText: 'Giờ'),
-              onChanged: (value) {
-                setState(() {
-                  time = value;
-                });
-              },
-              controller: TextEditingController(text: time),
-            ),
-          ],
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              Color(0xffB81736),
+              Color(0xff281537),
+            ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              _buildTextField(
+                label: 'Nội dung',
+                value: content,
+                onChanged: (value) {
+                  setState(() {
+                    content = value;
+                  });
+                },
+              ),
+              const SizedBox(height: 16),
+              _buildTextField(
+                label: 'Ngày',
+                value: date,
+                onChanged: (value) {
+                  setState(() {
+                    date = value;
+                  });
+                },
+              ),
+              const SizedBox(height: 16),
+              _buildTextField(
+                label: 'Giờ',
+                value: time,
+                onChanged: (value) {
+                  setState(() {
+                    time = value;
+                  });
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: () {
+          Navigator.pop(context, {
+            'content': content,
+            'ngày': date,
+            'giờ': time,
+          });
+        },
+        label: const Text(
+          'Lưu',
+          style: TextStyle(color: Colors.white),
+        ),
+        icon: const Icon(Icons.save,color: Colors.white,),
+        backgroundColor: Colors.deepPurple,
+      ),
+    );
+  }
+
+  Widget _buildTextField({
+    required String label,
+    required String value,
+    required Function(String) onChanged,
+  }) {
+    return Card(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10),
+      ),
+      elevation: 5,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16.0),
+        child: TextFormField(
+          decoration: InputDecoration(
+            labelText: label,
+            border: InputBorder.none,
+          ),
+          onChanged: onChanged,
+          controller: TextEditingController(text: value),
+          style: const TextStyle(fontSize: 16),
         ),
       ),
     );
